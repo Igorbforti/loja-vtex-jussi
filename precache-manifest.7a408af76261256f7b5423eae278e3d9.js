83687c29fa9e2ef9bca7b3c9560c7fb74213271d
Igor Forti <igbonuzzi@outlook.com>
2023-09-24 11:35:10 -0300

    Updates

---
+++ precache-manifest.7a408af76261256f7b5423eae278e3d9.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "1adb270a1bb9c8f42416",
+    "url": "/loja-vtex-jussi/static/css/main.a0fb9d97.chunk.css"
+  },
+  {
+    "revision": "1adb270a1bb9c8f42416",
+    "url": "/loja-vtex-jussi/static/js/main.1adb270a.chunk.js"
+  },
+  {
+    "revision": "3f0e6d4367d6a84bf16d",
+    "url": "/loja-vtex-jussi/static/js/1.3f0e6d43.chunk.js"
+  },
+  {
+    "revision": "45a6f073aa466e5fcf67",
+    "url": "/loja-vtex-jussi/static/js/runtime~main.45a6f073.js"
+  },
+  {
+    "revision": "0187bfc2df0cf757286fcdcfcdcea5ee",
+    "url": "/loja-vtex-jussi/static/media/jussi-logo.0187bfc2.svg"
+  },
+  {
+    "revision": "d49a3e9d888e67e72319df7c76ddb22d",
+    "url": "/loja-vtex-jussi/static/media/shopping-cart-icon.d49a3e9d.svg"
+  },
+  {
+    "revision": "31de20b1de8a3720cbdc7d6db1ebecb6",
+    "url": "/loja-vtex-jussi/static/media/menu-mobile.31de20b1.svg"
+  },
+  {
+    "revision": "bd12efc9e957fac45def3c4c4a8044b0",
+    "url": "/loja-vtex-jussi/static/media/banner-image1.bd12efc9.svg"
+  },
+  {
+    "revision": "0327db0e4c864c1f15fab88346894241",
+    "url": "/loja-vtex-jussi/static/media/banner-image2.0327db0e.svg"
+  },
+  {
+    "revision": "d3f657aeb5ff5264ab0214c3b4ee71d5",
+    "url": "/loja-vtex-jussi/static/media/banner-image3.d3f657ae.svg"
+  },
+  {
+    "revision": "e06925f4026db0f1352160510a40c2ec",
+    "url": "/loja-vtex-jussi/static/media/arrow-icon.e06925f4.svg"
+  },
+  {
+    "revision": "39d4c73c2ddd1adc810546179ff814f1",
+    "url": "/loja-vtex-jussi/static/media/logo-brastemp.39d4c73c.svg"
+  },
+  {
+    "revision": "98be60a32f14c28b8580ee77bd9520c4",
+    "url": "/loja-vtex-jussi/static/media/logo-compra-certa.98be60a3.svg"
+  },
+  {
+    "revision": "b0a14765024023e30b307160979fc6da",
+    "url": "/loja-vtex-jussi/static/media/logo-consul.b0a14765.svg"
+  },
+  {
+    "revision": "116fdf358f0fa817d8a7dd587a1fb36a",
+    "url": "/loja-vtex-jussi/static/media/logo-thebar.116fdf35.svg"
+  },
+  {
+    "revision": "1586ffb321b76307ef802faedc7c7a95",
+    "url": "/loja-vtex-jussi/static/media/stripes.1586ffb3.svg"
+  },
+  {
+    "revision": "56f60a829f11e81506daa7b4da0d7330",
+    "url": "/loja-vtex-jussi/static/media/shelf-image1.56f60a82.svg"
+  },
+  {
+    "revision": "84010f2ff5883e76282125fe695d0a9b",
+    "url": "/loja-vtex-jussi/static/media/shelf-image2.84010f2f.svg"
+  },
+  {
+    "revision": "671e97e3ef2d1349ea6d4219aa54a7c8",
+    "url": "/loja-vtex-jussi/static/media/shelf-image3.671e97e3.svg"
+  },
+  {
+    "revision": "4d30a390ef47d440ac0aeadf3fed960a",
+    "url": "/loja-vtex-jussi/static/media/shelf-image4.4d30a390.svg"
+  },
+  {
+    "revision": "643330927f88ec96db0e926cadab9cbe",
+    "url": "/loja-vtex-jussi/static/media/info-card-image.64333092.svg"
+  },
+  {
+    "revision": "94ae7f0d57d60e0fb10ecbb63984c315",
+    "url": "/loja-vtex-jussi/static/media/wppcompany.94ae7f0d.svg"
+  },
+  {
+    "revision": "c20e437bdfcdcb1837432b6b204ab024",
+    "url": "/loja-vtex-jussi/static/media/facebook.c20e437b.svg"
+  },
+  {
+    "revision": "5217daefe817a76004fba0bd11ae3aad",
+    "url": "/loja-vtex-jussi/static/media/instagram.5217daef.svg"
+  },
+  {
+    "revision": "06d077442f03dfe8ac161750cea5d993",
+    "url": "/loja-vtex-jussi/static/media/linkedin.06d07744.svg"
+  },
+  {
+    "revision": "01a1d012018a95da547f3d203e27eecc",
+    "url": "/loja-vtex-jussi/static/media/search-icon.01a1d012.svg"
+  },
+  {
+    "revision": "40f4c19cf46a2b0ec3df66cceb948748",
+    "url": "/loja-vtex-jussi/index.html"
+  }
+];
